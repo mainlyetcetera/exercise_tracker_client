392c003b03917a0c8cec763c56be969e7de016dd
Eric Campbell <eric.campbell@intevity.com>
2022-08-23 19:21:17 -0500

    prompt for and return exercise name

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 #!/usr/bin/ts-node
-
 import input from 'readline-sync'
-
-if (process) {
-console.log('have process')
-}
 
 console.log('Welcome to exercise client!')
 
@@ -37,39 +32,24 @@ ${queryResponses}
 console.log(q)
 
 const promptForExerciseName = (): string => {
-  const nameResponse = input.question('What was the exercise?')
-  console.log({ nameResponse })
+  const nameResponse = input.question('What was the exercise? ')
   switch (nameResponses[nameResponse]) {
     case nameResponses.a:
-      console.log(nameResponses.a)
       return nameResponses.a
     case nameResponses.c:
-      console.log(nameResponses.c)
       return nameResponses.c
     case nameResponses.r:
-      console.log(nameResponses.r)
       return nameResponses.r
     case nameResponses.l:
-      console.log(nameResponses.l)
       return nameResponses.l
     case nameResponses.q:
-      console.log(nameResponses.q)
       return nameResponses.q
     case nameResponses.h:
-      console.log(nameResponses.h)
       return nameResponses.h
     default:
-      console.log(`incorrect response given: ${nameResponses[nameResponse]}`)
       return `incorrect response given: ${nameResponses[nameResponse]}`
   }
 }
 
 const nameResult = promptForExerciseName()
 console.log({ nameResult })
-
-  // if (nameResponse && nameResponse === 'a') {
-  //   return nameResponses[nameResponse]
-  // } else {
-  //   return 'incorrect option provided'
-  // }
-// }
