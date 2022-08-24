2bc39d94849acd9e0465260fc1ae8081b4224ee6
Eric Campbell <eric.campbell@intevity.com>
2022-08-23 20:14:51 -0500

    chore: add basic test for weight

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,36 @@ import input from 'readline-sync'
 
 console.log('Welcome to exercise client!')
 
-type nmResp = {
-  [key: string]: string
-}
-
-const nameResponses: nmResp = {
-  a: 'bicep curls',
-  c: 'chest press',
-  r: 'rear-deltoid fly',
-  l: 'lat pull-down',
-  q: 'quad press',
-  h: 'hamstring press'
-}
-
-let queryResponses = '' 
-
-Object.keys(nameResponses).forEach(resp => {
-  if (nameResponses[resp]) {
-    queryResponses += `${resp}: ${nameResponses[resp]}\n`
+const promptForExerciseName = (): string => {
+  type nmResp = {
+    [key: string]: string
   }
-})
 
-const q = `
+  const nameResponses: nmResp = {
+    a: 'bicep curls',
+    c: 'chest press',
+    r: 'rear-deltoid fly',
+    l: 'lat pull-down',
+    q: 'quad press',
+    h: 'hamstring press'
+  }
+
+  let queryResponses = '' 
+
+  Object.keys(nameResponses).forEach(resp => {
+    if (nameResponses[resp]) {
+      queryResponses += `${resp}: ${nameResponses[resp]}\n`
+    }
+  })
+
+  const q = 
+`
 pick from following options:\n
 ${queryResponses}
 `
 
-console.log(q)
+  console.log(q)
 
-const promptForExerciseName = (): string => {
   const nameResponse = input.question('What was the exercise? ')
   switch (nameResponses[nameResponse]) {
     case nameResponses.a:
@@ -51,5 +52,27 @@ const promptForExerciseName = (): string => {
   }
 }
 
+const promptForExerciseWeight = (weightResponse: string = input.question('What was the weight for the set? ')): number | string => {
+  console.log('This needs to be an integer!')
+
+  if (parseInt(weightResponse)) {
+    return parseInt(weightResponse)
+  } else {
+    return 'Please enter an integer!'
+  }
+}
+
 const nameResult = promptForExerciseName()
 console.log({ nameResult })
+
+const numberResult = promptForExerciseWeight()
+console.log({ numberResult })
+
+type testParams = string | number | boolean
+
+const assert = (actual: testParams, expected: testParams, testName: string): boolean => {
+  console.log(`${testName} should be ${actual}, and got ${expected}`)
+  return actual === expected ? true : false
+}
+
+console.log(assert(promptForExerciseWeight('42'), 42, 'weight is the correct integer'))
